97baffe3521a79e761b9169029503d1dd0b61daf
CalebLewis0917 <caleblewis0917@gmail.com>
2022-10-01 15:53:27 -0500

    die

--- Cloud/main.py
+++ Cloud/main.py
@@ -2,11 +2,14 @@ import pyodbc
 
 # Functions
 def addReminder(n, d):
-    cursor.execute('INSERT INTO Reminders(isRead, name, details) VALUES(0, n, d')
+    reminder = 'INSERT INTO Reminders(isRead, name, details) VALUES(0,\'' + n + '\',\'' + d + '\')'
+    print(reminder)
+    cursor.execute(reminder)
 #def getReminder():
 
 #def markRead(n):
 
+print('begin')
 connection = pyodbc.connect('Driver={SQL Server};'
                             'Server=WINDELL-T2U0QEF;'
                             'Database=mydb;'
