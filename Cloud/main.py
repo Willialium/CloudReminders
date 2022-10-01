b7106c2c1e51ef553536321c9140c63d26421eb3
CalebLewis0917 <caleblewis0917@gmail.com>
2022-10-01 15:25:07 -0500

    die

--- Cloud/main.py
+++ Cloud/main.py
@@ -1,24 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'He, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-###### THIS IS FOR REAL ######
 import pyodbc
 
+# Functions
+
 connection = pyodbc.connect('Driver={SQL Server};'
-                            'Server=localhost;'
+                            'Server=WINDELL-T2U0QEF;'
                             'Database=mydb;'
                             'Trusted_Connection=True;')
+
+cursor = connection.cursor()
+insertFormula = 'INSERT INTO Reminders(name, details) VALUES(%s, %s)'
+reminder1 = ('Do the Dishes', 'i mean just figure it out')
+cursor.execute(insertFormula, reminder1)
+connection.commit()
+print('we made it')
