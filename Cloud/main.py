1fa5c1a20451fe35c8f0e6192437e1d1f46284e7
CalebLewis0917 <caleblewis0917@gmail.com>
2022-10-01 16:45:01 -0500

    die

--- Cloud/main.py
+++ Cloud/main.py
@@ -4,27 +4,15 @@ import pyodbc
 def addReminder(n, d):
     cursor.execute('INSERT INTO Reminders(isRead, name, details) VALUES(0,\'' + n + '\',\'' + d + '\')')
 def getReminders():
-    print('getting')
     cursor.execute('SELECT name, details FROM Reminders WHERE isRead = 0')
     return cursor.fetchall()
-
 def markRead(n):
-    print('marking')
     cursor.execute('UPDATE Reminders SET isRead = 1 WHERE name = \'' + n + '\'')
 
-print('begin')
+# Connects to Database
 connection = pyodbc.connect('Driver={SQL Server};'
                             'Server=WINDELL-T2U0QEF;'
                             'Database=mydb;'
                             'Trusted_Connection=True;')
 
 cursor = connection.cursor()
-allReminders = getReminders()
-for row in allReminders:
-    print(row)
-markRead('Do the Dishes')
-allReminders = getReminders()
-for row in allReminders:
-    print(row)
-connection.commit()
-print('we made it')
