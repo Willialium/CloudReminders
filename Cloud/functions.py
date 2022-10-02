44203c866e70d5c45a75f4262e4d3c460fbda207
Willialium <wilalatay@gmail.com>
2022-10-02 01:43:24 -0500

    Prototype canvas switcher

--- Cloud/functions.py
+++ Cloud/functions.py
@@ -4,11 +4,14 @@ import pyodbc
 def addReminder(n, d):
     cursor.execute('INSERT INTO Reminders(isRead, name, description) VALUES(0,\'' + n + '\',\'' + d + '\')')
     connection.commit()
-def getReminders():
-    cursor.execute('SELECT name, description FROM Reminders WHERE isRead = 0')
+def getNames():
+    cursor.execute('SELECT DISTINCT CONVERT(VARCHAR, name) FROM names')
+    return cursor.fetchall()
+def getReminders(name):
+    cursor.execute('SELECT name, description FROM Reminders WHERE isRead = 0 AND CONVERT(VARCHAR, name) = \'' + name + '\'')
     return cursor.fetchall()
 def markRead(n):
-    cursor.execute('UPDATE Reminders SET isRead = 1 WHERE CONVERT(VARCHAR, name) = \'' + n + '\'')
+    cursor.execute('UPDATE Reminders SET isRead = 0 WHERE CONVERT(VARCHAR, name) = \'' + n + '\'')
     connection.commit()
 
 # Connects to Database
--- GUI/mainGUI.py
+++ GUI/mainGUI.py
@@ -1,53 +1,100 @@
 import tkinter as tk
 from PIL import ImageTk, Image
-import pyodbc
+from functools import partial
 import Cloud.functions as cloud
 
+reminders = []
+checks = []
 
-reminders = cloud.getReminders()
+#########################
+#### MAKE MAIN FRAME ####
+#########################
 
 root = tk.Tk()
-root.title('Reminder')
-root.geometry('312x400')
-
 frame = tk.Frame(root, borderwidth=0, highlightthickness=0)
-frame.pack(fill='both',expand=1)
+nameCanvas = tk.Canvas(frame, bg='#800000', borderwidth=0, highlightthickness=0)
+remCanvas = tk.Canvas(frame, bg='#800000', borderwidth=0, highlightthickness=0)
+nameBG = ImageTk.PhotoImage(Image.open('name.png'))
+remBG = ImageTk.PhotoImage(Image.open('reminder.png'))
 
-canvas = tk.Canvas(frame, bg='#800000', borderwidth=0, highlightthickness=0)
-canvas.pack(side='left', fill='both', expand=1)
-scrollbar = tk.Scrollbar(frame, orient='vertical', command=canvas.yview)
-scrollbar.pack(side='right', fill='y')
 
-canvas.configure(yscrollcommand=scrollbar.set)
-canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion = canvas.bbox('all')))
-def _on_mouse_wheel(event):
-    canvas.yview_scroll(-1 * int((event.delta / 120)), "units")
-canvas.bind_all("<MouseWheel>", _on_mouse_wheel)
+def makeFrame():
+    root.title('Reminder')
+    root.geometry('312x400')
+
+    frame.pack(fill='both', expand=1)
+
+
+def showReminders(name):
+    global reminders
+    reminders = cloud.getReminders(name)
+    nameCanvas.children.destroy()
+    #makeRemCanvas()
+
+
+def _on_mouse_wheel_name(event):
+    nameCanvas.yview_scroll(-1 * int((event.delta / 120)), "units")
+
+
+def _on_mouse_wheel_rem(event):
+    nameCanvas.yview_scroll(-1 * int((event.delta / 120)), "units")
+
+
+def makeNameCanvas():
+    nameCanvas.pack(side='left', fill='both', expand=1)
+    scrollbar = tk.Scrollbar(frame, orient='vertical', command=nameCanvas.yview)
+    scrollbar.pack(side='right', fill='y')
+
+    nameCanvas.configure(yscrollcommand=scrollbar.set)
+    nameCanvas.bind('<Configure>', lambda e: nameCanvas.configure(scrollregion=nameCanvas.bbox('all')))
+
+    nameCanvas.bind_all("<MouseWheel>", _on_mouse_wheel_name)
+
+    names = [i for i in cloud.getNames()]
+    for i in range(len(names)):
+        nameCanvas.create_image(12, i * 90 + 8, image=nameBG, anchor='nw')
+        checks.append(tk.IntVar())
+        c = tk.Button(frame, text=names[i][0], bg='white', borderwidth=0, font=('Helvetica 15 bold'),
+                      width=22, height=2, command=partial(showReminders, names[i][0]))
+        nameCanvas.create_window(156, i * 90 + 20, anchor='n', window=c)
+
+
+def makeRemCanvas():
+    remCanvas.pack(side='left', fill='both', expand=1)
+    scrollbar = tk.Scrollbar(frame, orient='vertical', command=remCanvas.yview)
+    scrollbar.pack(side='right', fill='y')
+
+    remCanvas.configure(yscrollcommand=scrollbar.set)
+    remCanvas.bind('<Configure>', lambda e: remCanvas.configure(scrollregion=remCanvas.bbox('all')))
+
+    remCanvas.bind_all("<MouseWheel>", _on_mouse_wheel_rem)
+
+    for i in range(len(reminders)):
+        remCanvas.create_image(12, i * 130 + 10, image=remBG, anchor='nw')
+        checks.append(tk.IntVar())
+        c = tk.Checkbutton(frame, text=reminders[i][0], bg='white', font=14, variable=checks[i], onvalue=1, offvalue=0)
+        remCanvas.create_text(80, i * 130 + 50, text=reminders[i][1], font=9, width=180, anchor='nw')
+        remCanvas.create_window(30, i * 130 + 20, anchor='nw', window=c)
+
+
+################################
+#### HANDLE CLOSE PROTOCOLS ####
+################################
 
 def checkAction(name):
+    print(name)
     cloud.markRead(name)
 
-bg = ImageTk.PhotoImage(Image.open('reminder.png'))
-for i in range(len(reminders)):
-    canvas.create_image(12, i*130, image=bg, anchor='nw')
-    c = tk.Checkbutton(frame, text=reminders[i][0], bg='white', font=14, command=lambda: checkAction(reminders[i][0]))
-    canvas.create_text(80, i*130+45, text=reminders[i][1], font=9, width=180, anchor='nw')
-    checkCanvas = canvas.create_window(30, i*130 + 20, anchor='nw', window=c)
+
+def onClose():
+    for i in range(len(reminders)):
+        print(reminders[i][0], checks[i].get())
+        if int(checks[i].get()):
+            cloud.markRead(reminders[i][0])
+    root.destroy()
 
 
-
-
-#frontFrame = tk.Frame(canvas, bg='#800000', borderwidth=0, highlightthickness=0)
-#canvas.create_window((0,0), window=frontFrame, anchor='nw')
-
-#bg = ImageTk.PhotoImage(Image.open('reminder.png'))
-#for i in range(10):
-#    l = tk.Label(frontFrame, image=bg, borderwidth=0, highlightthickness=0).grid(row=i, column=0, pady=10, padx=12)
-#    f = tk.Frame(frontFrame, bg='#009000',borderwidth=0).grid(row=i, column=0, pady=10, padx=12)
-#    c = tk.Checkbutton(f, text='Check', bg='#FFFFFF', justify='left')
-
-
-
+makeFrame()
+makeNameCanvas()
+root.protocol("WM_DELETE_WINDOW", onClose)
 root.mainloop()
-
-
